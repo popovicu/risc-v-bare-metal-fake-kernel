b617fd2f057536285ab10c38200c7529b02f6ce0
uros <uros@uros-debian-desktop>
2023-09-20 20:56:44 -0700

    Print an easier way

--- hello_world_kernel.s
+++ hello_world_kernel.s
@@ -3,17 +3,9 @@
 
 _start: li a7, 0x4442434E
         li a6, 0x00
-1:      auipc a3, %pcrel_hi(debug_string)
-        addi a3, a3, %pcrel_lo(1b)
-        li a4, 0x00000000FFFFFFFF
-        li a5, 0xFFFFFFFF00000000
         li a0, 12
-        and a1, a3, a4
-        and a2, a3, a5
-        ecall
-
-        li a7, 0x01
-        mv a6, a0
+	lla a1, debug_string
+        li a2, 0
         ecall
 
 loop:   j loop
